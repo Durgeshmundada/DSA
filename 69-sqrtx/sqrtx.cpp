14e67ef8ababc6091e27bf41acb55a4d64d85332
Durgeshmundada <mundadadurgesh@gmail.com>
2025-08-15 18:04:14 +0530

    Time: 22 ms (5.03%) | Memory: 8.6 MB (50.74%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int a=pow(x,0.5);
-        return a;
+        long long i=0.0;
+        while(i*i<=x){
+            i++;
+        }
+    return i-1;
     }
+
 };
