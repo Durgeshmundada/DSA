8df2a5e090efa5fc1c105434c89930ce9310e85d
Durgeshmundada <mundadadurgesh@gmail.com>
2025-11-27 12:21:39 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (72.56%) - LeetSync

---
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> result;
+        int x=0;
+        for(int i=0;i<nums.size();i++){
+           x = (x * 2 + nums[i]) % 5;
+
+            if(x%5==0){
+                result.push_back(true);
+            }
+            else{
+                result.push_back(false);
+            }
+        }
+        return result;
+    }
+};
