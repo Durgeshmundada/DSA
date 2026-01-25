b0a940f2127654487210488bdccf00db56f6ccd3
Durgeshmundada <mundadadurgesh@gmail.com>
2026-01-25 14:18:36 +0530

    Time: 3 ms (30.79%) | Memory: 21.5 MB (93.92%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        return merge(intervals);
+        
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> result;
+        sort(intervals.begin(),intervals.end());
+        for(int i=0;i<intervals.size();i++){
+            int x=intervals[i][0];
+            int y=intervals[i][1];
+            while(i<intervals.size()-1 && (y>=intervals[i+1][0])){
+                i++;
+                y=max(y,intervals[i][1]);
+            }
+            result.push_back({x,y});
+        }
+        return result;
+    }
+};
