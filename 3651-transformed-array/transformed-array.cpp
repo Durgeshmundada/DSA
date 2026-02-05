fc583dfcd650171371d9e481d634d6e23b75989c
Durgeshmundada <mundadadurgesh@gmail.com>
2026-02-05 07:47:33 +0530

    Time: 8 ms (46.52%) | Memory: 25.6 MB (70.75%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+
+        for (int i = 0; i < n; i++) {
+            int shift = nums[i];
+            int idx = ((i + shift) % n + n) % n;  
+            result[i] = nums[idx];
+        }
+
+        return result;
+    }
+};
