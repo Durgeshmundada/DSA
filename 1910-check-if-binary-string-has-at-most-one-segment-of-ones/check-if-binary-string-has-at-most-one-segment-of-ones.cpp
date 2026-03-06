9ab3d22aa9014a90366760155590c734d4889e69
Durgeshmundada <mundadadurgesh@gmail.com>
2026-03-06 08:22:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (96.03%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        bool result=false;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='1' ){
-                if(result){
-                    return false;
-                }
-                else{
-                    while(i<s.size() && s[i]=='1' ){
-                        i++;
-                    }
-                    result=true;
-                }
-            }
+        if(s.find("01") != string::npos){
+            return false;
         }
-        return result;
+        else{
+           return true;
+        }
+        
     }
 };
