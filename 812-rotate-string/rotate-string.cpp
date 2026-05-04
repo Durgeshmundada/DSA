29c0525d2a859a4a66eca7347d9182ed35cff0ef
Durgeshmundada <mundadadurgesh@gmail.com>
2026-05-04 19:45:55 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (84.78%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size()){
+            return false;
+        }
+        string s1=s+s;
+        if(s1.find(goal)!=string::npos){
+            return true;
+        }
+        return false;
+    }
+};
