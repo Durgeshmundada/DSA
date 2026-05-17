fe9578ef06dfbffaba03b32a9d4122c1901be2d5
Durgeshmundada <mundadadurgesh@gmail.com>
2026-05-17 08:18:28 +0530

    Time: 0 ms (100.00%) | Memory: 36.3 MB (63.79%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
         vector<int> dp(arr.size(),0);
-        vector<int> dp1(arr.size(),-1);
-        return canr(arr,start,dp,dp1);
+        return canr(arr,start,dp);
     }
-    bool canr(vector<int>& arr,int start,vector<int>& dp,vector<int>& dp1){
-        if(dp1[start]!=-1) return dp1[start];
+    bool canr(vector<int>& arr,int start,vector<int>& dp){
         if(dp[start]==1) return false;
         if(arr[start]==0) return true;
         dp[start]=1;
-        return dp1[start]=(start-arr[start]>=0? canr(arr,start-arr[start],dp,dp1):false) || (start+arr[start]<arr.size()?canr(arr,start+arr[start],dp,dp1):false);
+        return (start-arr[start]>=0? canr(arr,start-arr[start],dp):false) || (start+arr[start]<arr.size()?canr(arr,start+arr[start],dp):false);
     }
 };
