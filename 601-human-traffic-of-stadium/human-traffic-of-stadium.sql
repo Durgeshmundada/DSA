606ca197d15ed53ba51eb9bd2853b34e5c7031d4
Durgeshmundada <mundadadurgesh@gmail.com>
2026-06-17 10:19:21 +0530

    Time: 360 ms (91.20%) | Memory: 0B (100.00%) - LeetSync

---
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -0,0 +1,15 @@
+SELECT id, visit_date, people
+FROM
+(
+    SELECT *,
+           LAG(people,1) OVER(ORDER BY id) AS p1,
+           LAG(people,2) OVER(ORDER BY id) AS p2,
+           LEAD(people,1) OVER(ORDER BY id) AS n1,
+           LEAD(people,2) OVER(ORDER BY id) AS n2
+    FROM Stadium
+) t
+WHERE
+      (people >= 100 AND p1 >= 100 AND p2 >= 100)
+   OR (people >= 100 AND p1 >= 100 AND n1 >= 100)
+   OR (people >= 100 AND n1 >= 100 AND n2 >= 100)
+ORDER BY visit_date;
