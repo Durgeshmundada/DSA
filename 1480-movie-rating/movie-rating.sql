71ce69b89681e766d1b899d3396e3beb28eeca9b
Durgeshmundada <mundadadurgesh@gmail.com>
2026-07-17 21:40:49 +0530

    Time: 1622 ms (13.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,20 @@
+(
+SELECT name AS results
+FROM Users
+JOIN MovieRating USING(user_id)
+GROUP BY user_id, name
+ORDER BY COUNT(*) DESC, name
+LIMIT 1
+)
+
+UNION ALL
+
+(
+SELECT title AS results
+FROM Movies
+JOIN MovieRating USING(movie_id)
+WHERE created_at LIKE '2020-02-%'
+GROUP BY movie_id, title
+ORDER BY AVG(rating) DESC, title
+LIMIT 1
+);
