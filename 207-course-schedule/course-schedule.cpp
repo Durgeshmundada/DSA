1faf6ee4827df8da37f8cfcc264e981ad98270ff
Durgeshmundada <mundadadurgesh@gmail.com>
2026-09-12 20:44:36 +0530

    Time: 8 ms (35.33%) | Memory: 19.3 MB (56.29%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool dfs(int i, vector<int>& vis, stack<int>& st,
+             vector<vector<int>>& adj) {
+
+        vis[i] = 1;
+
+        for (auto it : adj[i]) {
+
+            if (vis[it] == 1) {
+                return false;
+            }
+
+            if (vis[it] == 0) {
+                if (!dfs(it, vis, st, adj))
+                    return false;
+            }
+        }
+
+        vis[i] = 2;
+        st.push(i);
+
+        return true;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> adj(numCourses);
+
+        for (int i = 0; i < prerequisites.size(); i++) {
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+
+        vector<int> vis(numCourses, 0);
+
+        stack<int> st;
+
+        for (int i = 0; i < numCourses; i++) {
+
+            if (vis[i] == 0) {
+
+                if (!dfs(i, vis, st, adj))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
